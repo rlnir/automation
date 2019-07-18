e7301703e951d9bd09d1c77c0cb35faff71ec9ec
Nir <nir@clicktale.com>
2019-07-18 17:00:16 +0300

    react page updates

--- src/main/java/com/seleniumAssignment/pages/ReactExamplePage.java
+++ src/main/java/com/seleniumAssignment/pages/ReactExamplePage.java
@@ -1,7 +1,10 @@
 package com.seleniumAssignment.pages;
 
 import com.seleniumAssignment.common.BasePage;
+import com.seleniumAssignment.common.WebDriverUtils;
+import io.qameta.allure.Step;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
@@ -24,6 +27,9 @@ public class ReactExamplePage extends BasePage {
     @FindBy(className = "clear-completed")
     private WebElement clearCompleted;
 
+    @FindBy(id = "toggle-all")
+    private WebElement toggleAll;
+
     private static final String PAGE_PATH = "/examples/react/";
     private int todoCounter = 0;
 
@@ -31,6 +37,7 @@ public class ReactExamplePage extends BasePage {
         super(driver);
     }
 
+    @Step
     public void addTodoItem(String text) {
         newTodo.click();
         newTodo.sendKeys(text);
@@ -38,6 +45,7 @@ public class ReactExamplePage extends BasePage {
 
     }
 
+    @Step
     public void removeTodoItem(int idx) {
 
         if (getTodoListLength() > idx) {
@@ -47,6 +55,7 @@ public class ReactExamplePage extends BasePage {
         }
     }
 
+    @Step
     public void markTodoItem(int idx) {
 
         if (getTodoListLength() > idx) {
@@ -58,6 +67,26 @@ public class ReactExamplePage extends BasePage {
         }
     }
 
+    public void editItem(int idx, String text) {
+        if (getTodoListLength() > idx) {
+            List<WebElement> elements = driver.findElements(By.cssSelector("div[class=view]>label"));
+
+            WebDriverUtils.dbClick(driver, elements.get(idx));
+
+            WebElement el = driver.findElements(By.cssSelector("input[class=edit]")).get(idx);
+            el.click();
+            int len = el.getAttribute("value").length();
+            while (len > 0) {
+                el.sendKeys(Keys.BACK_SPACE);
+                len--;
+            }
+
+            el.sendKeys(text);
+            el.sendKeys(Keys.ENTER);
+        }
+    }
+
+    @Step
     public int getTodoListLength() {
 //        WebElement element = driver.findElement(By.className("todo-list"));
 //        List<WebElement> li = element.findElements(By.tagName("li"));
@@ -67,6 +96,7 @@ public class ReactExamplePage extends BasePage {
 
     }
 
+    @Step
     public int getCountText() {
         if (getTodoListLength() > 0) {
             WebElement element = driver.findElement(By.className("todo-count"));
@@ -76,6 +106,12 @@ public class ReactExamplePage extends BasePage {
         return 0;
     }
 
+    @Step
+    public void toggleAllItems() {
+        toggleAll.click();
+    }
+
+    @Step
     public void clickOnClearCompleted() {
         clearCompleted.click();
     }
