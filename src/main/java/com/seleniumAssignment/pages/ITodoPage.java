6460df23f505a40411ee13815230c6ecb84e7746
Nir <nir@clicktale.com>
2019-07-18 21:51:33 +0300

    interface for todo pages

---
+++ src/main/java/com/seleniumAssignment/pages/ITodoPage.java
@@ -0,0 +1,40 @@
+package com.seleniumAssignment.pages;
+
+import io.qameta.allure.Step;
+
+/**
+ * use this interface with TodoExamples pages for each language
+ */
+
+public interface ITodoPage {
+    @Step
+    void addTodoItem(String text);
+
+    @Step
+    void removeTodoItem(int idx);
+
+    @Step
+    void markTodoItem(int idx);
+
+    String getItemText(int idx);
+
+    void editItem(int idx, String text);
+
+    @Step
+    int getTodoListLength();
+
+    @Step
+    int getCountText();
+
+    @Step
+    void toggleAllItems();
+
+    @Step
+    void clickOnClearCompleted();
+
+    void filterCompleted();
+
+    void filterActive();
+
+    void filterAll();
+}
