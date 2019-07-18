4942852f85d112e415a06ab844a952f21a71cc77
Nir <nir@clicktale.com>
2019-07-18 16:59:40 +0300

    optimize imports

--- src/main/java/com/seleniumAssignment/common/DriverFactory.java
+++ src/main/java/com/seleniumAssignment/common/DriverFactory.java
@@ -1,6 +1,5 @@
 package com.seleniumAssignment.common;
 
-import com.seleniumAssignment.utils.ProjectTestProps;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
