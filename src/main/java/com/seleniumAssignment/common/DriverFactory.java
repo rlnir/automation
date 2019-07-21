6110c2dd406c3ae058d8b65dc1ca80ea3d039b67
Nir <nirharel>
2019-07-21 11:05:44 +0300

    add bs driver + refactor

--- src/main/java/com/seleniumAssignment/common/DriverFactory.java
+++ src/main/java/com/seleniumAssignment/common/DriverFactory.java
@@ -1,21 +1,55 @@
-package com.seleniumAssignment.common;
+package com.seleniumassignment.common;
 
+import com.seleniumassignment.utils.ProjectTestProps;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 class DriverFactory {
-    static WebDriver getBrowser(String browserName) {
+    static WebDriver getBrowserDriver(String browserName) {
         WebDriver driver;
-        if (browserName != null && browserName.equals("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-
-        } else {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
         }
         return driver;
     }
+
+    static RemoteWebDriver getBrowserStackDriver(String browserName) {
+        RemoteWebDriver driver;
+        String url = "https://" + ProjectTestProps.getBrowserStackUser() + ":" + ProjectTestProps.getBrowserStackKey() + "@" + ProjectTestProps.getBrowserStackHub();
+        try {
+            switch (browserName) {
+                case "chrome": default:
+                    driver = new RemoteWebDriver(
+                            new URL(url),
+                            DesiredCapabilities.chrome()
+                    );
+                    return driver;
+                case "firefox":
+                    driver = new RemoteWebDriver(
+                            new URL(url),
+                            DesiredCapabilities.firefox()
+                    );
+                    return driver;
+            }
+        } catch (MalformedURLException e) {
+            System.out.println("BS Url is incorrect");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
